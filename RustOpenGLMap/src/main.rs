3b71b922b31c8b3072922fd12583e9ea29e580ef
biopicDecks <biopic.decks-0w@icloud.com>
2025-05-10 19:48:43 +0100

    first working version
    Signed-off-by: biopicDecks <biopic.decks-0w@icloud.com>

--- RustOpenGLMap/src/main.rs
+++ RustOpenGLMap/src/main.rs
@@ -4,7 +4,6 @@ use sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::video::{self, GLContext};
-
 type Vertex = [f32; 3];
 const VERTICES: [Vertex; 3] = [[-0.5, -0.5, 0.0], [0.5, -0.5, 0.0], [0.0, 0.5, 0.0]];
 
@@ -12,6 +11,14 @@ const VERT_SHADER: &str = r#"#version 410 core
   layout (location = 0) in vec3 pos;
   void main() {
     gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+  }
+"#;
+
+const FRAG_SHADER: &str = r#"#version 410 core
+  out vec4 final_color;
+
+  void main() {
+    final_color = vec4(1.0, 0.5, 0.2, 1.0);
   }
 "#;
 
@@ -41,21 +48,49 @@ fn main() -> Result<(), String> {
     // compile vertex shader
 
     unsafe {
+        // Now calls like gl::ClearColor should be recognized
+        gl::ClearColor(0.7, 0.1, 0.5, 1.0);
+        // gl::COLOR_BUFFER_BIT comes from gl::types::GLenum
+
+        let mut vao = 0;
+        gl::GenVertexArrays(1, &mut vao);
+        assert_ne!(vao, 0);
+        gl::BindVertexArray(vao);
+
+        let mut vbo = 0;
+        gl::GenBuffers(1, &mut vbo);
+        assert_ne!(vbo, 0);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            size_of_val(&VERTICES) as isize,
+            VERTICES.as_ptr().cast(),
+            gl::STATIC_DRAW,
+        );
+
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            size_of::<Vertex>().try_into().unwrap(),
+            0 as *const _,
+        );
+        gl::EnableVertexAttribArray(0);
+
+        // Vertex Shader
         let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
         assert_ne!(vertex_shader, 0);
-
         gl::ShaderSource(
             vertex_shader,
             1,
             &(VERT_SHADER.as_bytes().as_ptr().cast()),
             &(VERT_SHADER.len().try_into().unwrap()),
         );
-
         gl::CompileShader(vertex_shader);
 
         let mut success = 0;
         gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
-
         if success == 0 {
             let mut v: Vec<u8> = Vec::with_capacity(1024);
             let mut log_len = 0_i32;
@@ -67,30 +102,17 @@ fn main() -> Result<(), String> {
         }
 
         // compile fragment shader
-
         let frag_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
         assert_ne!(frag_shader, 0);
-
-        const FRAG_SHADER: &str = r#"#version 410 core
-  out vec4 final_color;
-
-  void main() {
-    final_color = vec4(1.0, 0.5, 0.2, 1.0);
-  }
-"#;
-
         gl::ShaderSource(
             frag_shader,
             1,
             &(FRAG_SHADER.as_bytes().as_ptr().cast()),
             &(FRAG_SHADER.len().try_into().unwrap()),
         );
-
         gl::CompileShader(frag_shader);
-
         success = 0;
         gl::GetShaderiv(frag_shader, gl::COMPILE_STATUS, &mut success);
-
         if success == 0 {
             let mut v: Vec<u8> = Vec::with_capacity(1024);
             let mut log_len = 0_i32;
@@ -101,11 +123,11 @@ fn main() -> Result<(), String> {
             println!("Fragment Shader Compiled Succccesfully");
         }
 
+        // create shader programme
         let shader_program = gl::CreateProgram();
         gl::AttachShader(shader_program, vertex_shader);
         gl::AttachShader(shader_program, frag_shader);
         gl::LinkProgram(shader_program);
-
         success = 0;
         gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success);
         if success == 0 {
@@ -120,12 +142,9 @@ fn main() -> Result<(), String> {
         // clean up
         gl::DeleteShader(vertex_shader);
         gl::DeleteShader(frag_shader);
-    }
 
-    unsafe {
-        // Now calls like gl::ClearColor should be recognized
-        gl::ClearColor(0.7, 0.1, 0.5, 1.0);
-        // gl::COLOR_BUFFER_BIT comes from gl::types::GLenum
+        // set program
+        gl::UseProgram(shader_program);
     }
 
     'running: loop {
@@ -141,43 +160,10 @@ fn main() -> Result<(), String> {
         }
 
         unsafe {
-            let mut vao = 0;
-            gl::GenVertexArrays(1, &mut vao);
-            assert_ne!(vao, 0);
-        }
-
-        unsafe {
-            let mut vbo = 0;
-            gl::GenBuffers(1, &mut vbo);
-            assert_ne!(vbo, 0);
-
-            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-
-            gl::BufferData(
-                gl::ARRAY_BUFFER,
-                size_of_val(&VERTICES) as isize,
-                VERTICES.as_ptr().cast(),
-                gl::STATIC_DRAW,
-            );
-        }
-
-        unsafe {
-            gl::VertexAttribPointer(
-                0,
-                3,
-                gl::FLOAT,
-                gl::FALSE,
-                size_of::<Vertex>().try_into().unwrap(),
-                0 as *const _,
-            );
-            gl::EnableVertexAttribArray(0);
-
             gl::Clear(gl::COLOR_BUFFER_BIT);
             gl::DrawArrays(gl::TRIANGLES, 0, 3);
         }
-
         window.gl_swap_window();
-
         ::std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / 60));
     }
 
